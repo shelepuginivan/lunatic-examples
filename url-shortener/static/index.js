7e7fc04f33fb37408b48b729dc937c88ab6671e1
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-27 15:33:13 +0300

    feat: handle errors

--- url-shortener/static/index.js
+++ url-shortener/static/index.js
@@ -1,20 +1,50 @@
+const clipButton = document.getElementById('clip-button');
 const form = document.getElementById('form');
-const result = document.getElementById('result')
+const resultWrapper = document.getElementById('result-wrapper');
+const result = document.getElementById('result');
+const message = document.getElementById('message');
 
 form.onsubmit = async (event) => {
 	event.preventDefault();
 
 	const url = event.target.url.value;
+	try {
+		const res = await fetch('/', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ url })
+		});
 
-	const res = await fetch('/', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ url })
-	});
-
-	const body = await res.json();
-	result.innerText = body.url;
+		const body = await res.json();
+		const shortUrl = body.url;
+		
+		resultWrapper.dataset.visible = "true";
+		result.innerText = `Your URL: ${shortUrl}`;
+		clipButton.onclick = () => copyToClipboard(shortUrl);
+	} catch {
+		message.innerText = 'An error occurred! Please try later';
+		message.dataset.status = 'error';
+		return;
+	}
 }
+
+const copyToClipboard = async (text) => {
+	if (!navigator.clipboard) {
+		message.innerText = 'Your browser does not support this!';
+		message.dataset.status = 'error';
+		return;
+	}
+
+	try {
+		await navigator.clipboard.writeText(text);
+		message.innerText = 'Copied to clipboard!'
+		message.dataset.status = 'success';
+	} catch {
+		errorMessage.innerText = 'Your browser does not support this!';
+		message.dataset.status = 'error';
+	}
+}
+
