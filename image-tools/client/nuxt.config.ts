5f178e89702ff70899c5d0c3e33d1d0c763a521f
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 01:20:19 +0300

    feat(config): configure Nuxt app

--- image-tools/client/nuxt.config.ts
+++ image-tools/client/nuxt.config.ts
@@ -1,7 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default {
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', type: 'image/png', href: '/favicon.png' }
+			]
+		}
+	},
+	css: ['@/assets/main.scss'],
 	devtools: { enabled: true },
+	runtimeConfig: {
+		public: {
+			apiBaseUrl: process.env.API_BASE_URL ?? 'http://localhost:8000'
+		}
+	},
 	modules: [
-		'@nuxtjs/eslint-module',
-	]
+		'@nuxtjs/google-fonts',
+		'nuxt-icon'
+	],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@import \'@/assets/global.scss\';\n'
+				}
+			}
+		}
+	},
+	googleFonts: {
+		families: {
+			Montserrat: true
+		}
+	}
 };
