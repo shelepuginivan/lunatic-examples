8ad824e3b0fc0d205f904af88b3ed3ad27b71fd9
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-28 11:20:32 +0300

    feat: validate incoming data

--- url-shortener/src/main.ts
+++ url-shortener/src/main.ts
@@ -4,6 +4,9 @@ import { nanoid } from 'nanoid';
 import { join } from 'path';
 import { render } from 'pug';
 import { createClient } from 'redis';
+import { ZodError } from 'zod';
+
+import { urlCreationSchema } from './schemas';
 
 const app = new LunaticServer();
 const redisClient = createClient();
@@ -32,8 +35,10 @@ app
 		await res.status(302).redirect(originalUrl);
 	})
 	.post('/', async (req, res) => {
-		const url = (req.body as Record<'url', string>).url;
-		let id = nanoid(7);
+		try {
+			const { url } = urlCreationSchema.parse(req.body);
+
+			let id = nanoid(7);
 
 			while (await redisClient.get(id)) {
 				id = nanoid(7);
@@ -41,8 +46,14 @@ app
 
 			await redisClient.set(id, url);
 
-		const shortUrl = `${process.env.HOST}/${id}`;
-		await res.status(200).json({ url: shortUrl });
+			const shortUrl = `${process.env.HOST}/${id}`;
+			await res.status(200).json({ url: shortUrl });
+		} catch (error) {
+			if (error instanceof ZodError) {
+				await res.status(400).json({ message: 'URL is not valid' });
+			}
+		}
+
 	})
 	.use('/', serveStatic(join(__dirname, '..', 'static')));
 
