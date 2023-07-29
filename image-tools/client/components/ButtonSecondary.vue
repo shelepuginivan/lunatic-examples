340f8f7a055d37b84434389b0a76d0ae1cb78de6
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:20:46 +0300

    feat(components): add component ButtonSecondary

---
+++ image-tools/client/components/ButtonSecondary.vue
@@ -0,0 +1,23 @@
+<template>
+	<button class='button'>
+		<slot></slot>
+	</button>
+</template>
+
+<style scoped lang='scss'>
+.button {
+	transition: $transition;
+	display: flex;
+	align-items: center;
+	gap: 4px;
+	background-color: $darkgray;
+	padding: 8px 16px;
+	cursor: pointer;
+	border-radius: 4px;
+
+	&: hover {
+		background-color: $gray;
+	}
+}
+
+</style>
