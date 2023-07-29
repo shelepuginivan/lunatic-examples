b7870311be6b595d41bfb5e55aa97f8da28a7d45
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 12:18:04 +0300

    feat(components): add component LinkButton

---
+++ image-tools/client/components/LinkButton.vue
@@ -0,0 +1,31 @@
+<template>
+	<a
+		class='link-button'
+		:href='href'
+	>
+		<slot/>
+	</a>
+</template>
+
+<script setup lang='ts'>
+defineProps<{ href: string }>();
+</script>
+
+<style scoped lang='scss'>
+.link-button {
+	transition: $transition;
+	display: flex;
+	align-items: center;
+	gap: 4px;
+	background-color: $darkgray;
+	padding: 8px 16px;
+	cursor: pointer;
+	border-radius: 4px;
+	text-decoration: none;
+
+	&:hover {
+		background-color: $gray;
+	}
+}
+
+</style>
