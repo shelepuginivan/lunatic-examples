69b4488ff844202bbe3319ed25cf73096bf8772f
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:56:40 +0300

    feat(components): add component ImagePreview

---
+++ image-tools/client/components/ImagePreview.vue
@@ -0,0 +1,21 @@
+<template>
+	<img
+		class='image-preview'
+		:src='src'
+		alt='Image preview'
+	>
+</template>
+
+<script setup lang='ts'>
+defineProps<{ src: string }>();
+</script>
+
+<style lang='scss'>
+.image-preview {
+	border: 1px solid $lightgray;
+	border-radius: 8px;
+	max-height: 300px;
+	max-width: 300px
+}
+
+</style>
