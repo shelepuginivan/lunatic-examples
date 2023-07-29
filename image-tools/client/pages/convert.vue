2a261ce94c59838cefbbaca397b9ca4a75f7f490
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 12:52:21 +0300

    feat(pages): add /convert page

--- image-tools/client/pages/convert.vue
+++ image-tools/client/pages/convert.vue
@@ -1,11 +1,113 @@
 <template>
-	<h1>Convert</h1>
+	<div class='wrapper'>
+		<NavHeader/>
+		<h1 
+			class='page-header'
+			:data-status='status'
+		>
+			{{ headerText }}
+		</h1>
+		<main class='main'>
+			<form
+				class='convert-form'
+				@submit.prevent='onSubmit'
+			>
+				<ImagePicker name='image'/>
+				<TextInput
+					name='format'
+					placeholder='Format (avif, png...)'
+				/>
+				<ButtonPrimary>Submit</ButtonPrimary>
+			</form>
+			<div class='result'>
+				<div
+					class='success'
+					v-if='data'
+				>
+					<ImagePreview
+						v-if='data'
+						:src='imageUrl'
+					/>
+					<LinkButton download :href='imageUrl'>
+						Download image
+					</LinkButton>
+				</div>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script setup lang='ts'>
+const { mutate: uploadImage, data, status } = useConvertImageMutation();
+
+const onSubmit = async (event: SubmitEvent) => {
+	const form = event.target as HTMLFormElement;
+	const body = new FormData(form);
+
+	await uploadImage(body);
+}
+
+const headerText = computed(() => {
+	switch (status.value) {
+		case 'loading':
+			return 'Please wait...';
+		case 'success':
+			return 'Success!';
+		case 'error':
+			return 'Something went wrong...';
+		default:
+			return 'Convert Image';
+	}
+});
+
+const imageUrl = computed(() => {
+	if (!data) {
+		return null;
+	}
+
+	return URL.createObjectURL(data.value);
+});
 
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+.wrapper {
+	padding-top: 60px;
+}
+
+.page-header {
+	padding-left: 48px;
+
+	&[data-status='success'] {
+		@extend .gradient-font;
+	}
+
+	&[data-status='error'] {
+		color: $red;
+	}
+}
+
+.main {
+	display: flex;
+	padding: 24px;
+}
+
+.convert-form,
+.result {
+	display: flex;
+	flex-direction: column;
+	width: 50%;
+	padding: 10px 24px;
+	gap: 10px;
+}
+
+.success {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	gap: 12px;
+}
 
 </style>
+
