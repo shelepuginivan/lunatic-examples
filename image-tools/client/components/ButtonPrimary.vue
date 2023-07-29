b37331958a8d05aaa452f45885eaea6870f1cf0f
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:20:38 +0300

    feat(components): add component ButtonPrimary

---
+++ image-tools/client/components/ButtonPrimary.vue
@@ -0,0 +1,24 @@
+<template>
+	<button class='button'>
+		<slot></slot>
+	</button>
+</template>
+
+<style scoped lang='sass'>
+.button
+	transition: $transition
+	display: flex
+	align-items: center
+	justify-content: center
+	gap: 4px
+	padding: 16px
+	border: 2px solid $lightgreen
+	color: $lightgreen
+	background-color: transparent
+	cursor: pointer
+	border-radius: 12px
+
+	&:hover
+		background-color: $lightgreen
+		color: $black
+</style>
