8e5717cabec12438abe4ab1031e4324e72e9ccee
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:22:16 +0300

    feat(components): add component NavHeader

---
+++ image-tools/client/components/NavHeader.vue
@@ -0,0 +1,32 @@
+<template>
+	<header class='header'>
+		<NuxtLink class='main-link' to='/'>
+			<IconCSS class='icon-logo' name='lucide:image'/>
+			Image Tools
+		</NuxtLink>
+	</header>
+</template>
+
+<style scoped lang='scss'>
+.header {
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	position: fixed;
+	left: 0;
+	top: 0;
+	height: 60px;
+	padding-inline: 24px;
+}
+
+.icon-logo {
+	color: $yellow;
+}
+
+.main-link {
+	@extend .gradient-font;
+	text-decoration: none;
+
+}
+
+</style>
