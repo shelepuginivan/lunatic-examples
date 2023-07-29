f9e6ef6e3a2cbd96b56f00eaae882befe2e4ddee
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:14:16 +0300

    feat: add cors

--- image-tools/server/src/main.ts
+++ image-tools/server/src/main.ts
@@ -1,9 +1,12 @@
-import { formParser, LunaticServer } from '@shelepuginivan/lunatic';
+import { cors, formParser, LunaticServer } from '@shelepuginivan/lunatic';
 
 import { imageRouter } from './image/image.router';
 
 const app = new LunaticServer();
 
+app.use(cors({
+	origin: 'http://localhost:3000'
+}))
 app.use(formParser);
 app.use('/image', imageRouter);
 
