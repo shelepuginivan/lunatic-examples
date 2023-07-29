4028da5e1fa8dc011de16b07500c3561e53a4598
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:40:49 +0300

    feat(components): add component TextInput

---
+++ image-tools/client/components/TextInput.vue
@@ -0,0 +1,18 @@
+<template>
+	<input type='text' class='text-input'/>
+</template>
+
+<style scoped lang='scss'>
+.text-input {
+	transition: $transition;
+	padding: 4px 12px;
+	background-color: transparent;
+	border: 1px solid $lightgray;
+	border-radius: 4px;
+
+	&:focus,
+	&:focus-within {
+		border-color: $lightgreen;
+	}
+}
+</style>
