d23e3436cafd2cfb7fb38db0f25f5c1a5d0403df
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-29 11:38:33 +0300

    fix(style): use scss instead of sass

--- image-tools/client/components/ButtonPrimary.vue
+++ image-tools/client/components/ButtonPrimary.vue
@@ -4,21 +4,24 @@
 	</button>
 </template>
 
-<style scoped lang='sass'>
-.button
-	transition: $transition
-	display: flex
-	align-items: center
-	justify-content: center
-	gap: 4px
-	padding: 16px
-	border: 2px solid $lightgreen
-	color: $lightgreen
-	background-color: transparent
-	cursor: pointer
-	border-radius: 12px
+<style scoped lang='scss'>
+.button {
+	transition: $transition;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 4px;
+	padding: 16px;
+	border: 2px solid $lightgreen;
+	color: $lightgreen;
+	background-color: transparent;
+	cursor: pointer;
+	border-radius: 12px;
 
-	&:hover
-		background-color: $lightgreen
-		color: $black
+	&:hover {
+		background-color: $lightgreen;
+		color: $black;
+	}
+}
+
 </style>
