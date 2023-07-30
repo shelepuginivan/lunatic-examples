64e0f7da67acc5fc28bdba4c27f154c38ede62ff
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 08:13:36 +0300

    feat: change signature of .svg method
    Make FormatService.svg method async

--- nine-ball-api/src/format/format.ts
+++ nine-ball-api/src/format/format.ts
@@ -2,7 +2,7 @@ export interface FormatService {
 	html(answer: string): string
 	image(answer: string): Promise<Buffer>
 	json(answer: string): { answer: string }
-	svg(answer: string): string
+	svg(answer: string): Promise<string>
 	text(answer: string): string
 	xml(answer: string): string
 }
