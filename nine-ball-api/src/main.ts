d191817d3a5277493ca1c646cb89a703fcbc5c58
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 16:07:24 +0300

    feat(api): add rate limiter middleware

--- nine-ball-api/src/main.ts
+++ nine-ball-api/src/main.ts
@@ -1,9 +1,20 @@
-import { LunaticServer } from '@shelepuginivan/lunatic';
+import { cookieParser, LunaticServer } from '@shelepuginivan/lunatic';
 
 import { apiRouter } from './api/api.router';
 
 const app = new LunaticServer();
 
+app.use(cookieParser);
+app.use('/api/*', (req, res, next) => {
+	if (req.cookies?.exceeded) {
+		return res.status(403).json({
+			message: 'You cannot ask Cirno a question more than once every 9 minutes.'
+		});
+	}
+
+	res.setCookie('exceeded', 'true', { httpOnly: true, maxAge: 9 * 60 });
+	return next();
+});
 app.use('/api', apiRouter);
 
 const main = () => {
