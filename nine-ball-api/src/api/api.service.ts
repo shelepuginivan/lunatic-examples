85a48b38f5b0a0f38b8913ac7258396ed9e7d78a
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 05:34:15 +0300

    feat: add ApiService implementation

---
+++ nine-ball-api/src/api/api.service.ts
@@ -0,0 +1,65 @@
+import { CirnoService } from '../cirno/cirno'
+import { FormatService } from '../format/format'
+import { ApiService } from './api'
+
+export class ApiServiceImpl implements ApiService {
+	constructor(
+		private readonly cirnoService: CirnoService,
+		private readonly formatService: FormatService
+	) {}
+
+	html(question: string, locale = 'en'): string {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.html(answer);
+	}
+
+	async image(question: string, locale = 'en'): Promise<Buffer> {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.image(answer);
+	}
+
+	json(question: string, locale = 'en'): { answer: string } {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.json(answer);
+	}
+
+	svg(question: string, locale = 'en'): string {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.svg(answer);
+	}
+
+	text(question: string, locale = 'en'): string {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.text(answer);
+	}
+
+	xml(question: string, locale = 'en'): string {
+		const answer = this.cirnoService.generateAnswer(
+			question,
+			locale
+		);
+
+		return this.formatService.xml(answer);
+	}
+}
+
