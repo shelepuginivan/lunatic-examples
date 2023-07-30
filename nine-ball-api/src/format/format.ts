44795111d286db283e86d52b19aa8d1b70010280
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 04:05:17 +0300

    feat: change signature of FormatService.image()
    Make method FormatService.image() async

--- nine-ball-api/src/format/format.ts
+++ nine-ball-api/src/format/format.ts
@@ -1,6 +1,6 @@
 export interface FormatService {
 	html(answer: string): string
-	image(answer: string): Buffer
+	image(answer: string): Promise<Buffer>
 	json(answer: string): { answer: string }
 	svg(answer: string): string
 	text(answer: string): string
