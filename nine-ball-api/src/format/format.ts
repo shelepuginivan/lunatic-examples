6c814de15a3e5c29ffd1579624a8debd044a2ce0
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 20:31:14 +0300

    feat: make FormatService.text method async

--- nine-ball-api/src/format/format.ts
+++ nine-ball-api/src/format/format.ts
@@ -3,7 +3,7 @@ export interface FormatService {
 	image(answer: string): Promise<Buffer>
 	json(answer: string): { answer: string }
 	svg(answer: string): Promise<string>
-	text(answer: string): string
+	text(answer: string): Promise<string>
 	xml(answer: string): string
 }
 
