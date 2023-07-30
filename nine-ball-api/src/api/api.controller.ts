361625ba3145d8d179009382adf4abbab6a5fda7
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-30 06:54:15 +0300

    refactor: set default locale
    Use default locale if specified one is not valid. Remove locale type
    validation

--- nine-ball-api/src/api/api.controller.ts
+++ nine-ball-api/src/api/api.controller.ts
@@ -8,66 +8,66 @@ export class ApiControllerImpl implements ApiController {
 	html(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const body = this.apiService.html(q, hl);
+		const body = this.apiService.html(q, String(hl));
 		return res.status(200).send(body, 'text/html');
 	}
 
 	async image(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const image = await this.apiService.image(q, hl);
+		const image = await this.apiService.image(q, String(hl));
 		return res.status(200).send(image, 'image/png');
 	}
 	
 	json(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const body = this.apiService.json(q, hl);
+		const body = this.apiService.json(q, String(hl));
 		return res.status(200).json(body);
 	}
 
 	svg(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const svgImage = this.apiService.svg(q, hl);
+		const svgImage = this.apiService.svg(q, String(hl));
 		return res.status(200).send(svgImage, 'image/svg+xml');
 	}
 
 	text(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const text = this.apiService.text(q, hl);
+		const text = this.apiService.text(q, String(hl));
 		return res.status(200).text(text);
 	}
 
 	xml(req: Request, res: Response) {
 		const { q, hl } = req.query;
 
-		if (typeof q !== 'string' || typeof hl !== 'string') {
+		if (typeof q !== 'string') {
 			return res.status(400).json({ message: 'invalid data' });
 		}
 
-		const xml = this.apiService.html(q, hl);
+		const xml = this.apiService.html(q, String(hl));
 		return res.status(200).send(xml, 'application/xml');
 	}
 }
