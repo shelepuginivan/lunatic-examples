13a7f125986f75b65df31d6b22d0873267b02f90
shelepuginivan <shelepuginivanm@gmail.com>
2023-08-03 12:31:10 +0300

    feat: add endpoint for process killing

--- web-htop/src/main.ts
+++ web-htop/src/main.ts
@@ -1,4 +1,5 @@
 import { serveStatic, LunaticServer } from '@shelepuginivan/lunatic';
+import fkill from 'fkill';
 import { createServer } from 'http';
 import psList from 'ps-list';
 import { Server } from 'socket.io';
@@ -6,6 +7,18 @@ import { Server } from 'socket.io';
 const app = new LunaticServer();
 const httpServer = createServer(app.callback);
 const io = new Server(httpServer);
+
+app.post('/kill/:pid', async (req, res) => {
+	const { pid: pidString } = req.params;
+	const pid = Number(pidString);
+
+	if (isNaN(pid)) {
+		return await res.status(400).json({ message: 'invalid pid' });
+	}
+
+	await fkill(pid, { force: true });
+	await res.status(200).json({ message: 'target eliminated' });
+});
 
 app.use('/', serveStatic('static'));
 
